707335892ba2b06000507bcfad5f52fc7844e168
graziacrp <graziacarpineta25@gmail.com>
2022-11-14 17:26:28 +0100

    ok

--- snack 6/script.js
+++ snack 6/script.js
@@ -5,3 +5,24 @@ function casuale() {
 
 
 Math.round(Math.random()*9)+1;
+
+
+
+
+function casuali() {
+    n = parseInt(document.getElementById("quanti").value);
+    if (n > 0 && n <= 10) {
+      str = "";
+      for (i = 0; i < n; i++) {
+           num = Math.round(Math.random()*10 + 1);
+           if (i > 0) {
+              str += ", ";
+           }
+           str += num;
+       }
+     document.getElementById("casuali").innerHTML = "Numeri generati: " + str;
+     } 
+     else {
+        document.getElementById("casuali").innerHTML = "Inserisci un numero maggiore di 0 e inferiore a 10";
+     }
+  }
