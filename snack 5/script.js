6207fef6d176e1a7835da2ab88c1488018fc2d93
graziacrp <graziacarpineta25@gmail.com>
2022-11-14 16:31:21 +0100

    ok

---
+++ snack 5/script.js
@@ -0,0 +1,13 @@
+
+let arrNum = [];
+
+
+for(let i = 0; i < 6; i++) {
+    let num = parseInt(prompt("Inserisci un numero"));
+
+    if(num % 2 !== 0){
+        arrNum.push(num);
+    }
+}
+console.log(arrNum);
+
